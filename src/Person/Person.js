1f86e0d22c3f27cca7f0e355dd75b5828da0d791
theiconik <kumarujjawal51@gmail.com>
2021-03-15 00:00:29 +0530

    Outputting Dynamic content in JSX

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const person = () => {
-   return <p>I'm Ujjawal Pandey.</p>
+   return <p>I'm Ujjawal Pandey. And I am {Math.floor(Math.random()*30)} years old!</p>
 }
 
 export default person;
