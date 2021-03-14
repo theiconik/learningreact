b41ac644c4677357e775929b88cc19be83f42c7b
theiconik <kumarujjawal51@gmail.com>
2021-03-15 00:13:49 +0530

    Props Children

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const person = (props) => {
-   return <p>I'm {props.name} . And I am {props.age} years old!</p>
+   return (
+      <div>
+         <p>I'm {props.name} . And I am {props.age} years old!</p>
+         <p>{props.children}</p>
+      </div>
+   )
 }
 
 export default person;
