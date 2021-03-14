65298a435feadeb6729aa857730aa3b6222194b4
theiconik <kumarujjawal51@gmail.com>
2021-03-15 00:14:48 +0530

    Functional Component in React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello World, this is Iconik.</h1>
+        <Person name="Ujjawal" age="20"/>
+        <Person name="Ujjwal" age="21">He'll never have sex.</Person>
+        <Person name="Ritik" age="20"/>
       </div>
     );
   }
