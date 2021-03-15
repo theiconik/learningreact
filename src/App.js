4a49afdcb0ee2ce41dc62b33be27fd80a4e69188
theiconik <kumarujjawal51@gmail.com>
2021-03-15 19:33:50 +0530

    State in Component Class

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons : [
+      {name: 'Ujjawal', age: 20},
+      {name: 'Ujjwal', age: 21},
+      {name: 'Ritik', age: 20}
+    ]
+  }
+  
   render() {
     return (
       <div className="App">
         <h1>Hello World, this is Iconik.</h1>
-        <Person name="Ujjawal" age="20"/>
-        <Person name="Ujjwal" age="21">He'll never have sex.</Person>
-        <Person name="Ritik" age="20"/>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>He'll never have sex.</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
