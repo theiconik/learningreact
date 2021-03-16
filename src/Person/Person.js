9b9814bb0da791ffcfa161d106b80084164b9a57
theiconik <kumarujjawal51@gmail.com>
2021-03-16 20:51:56 +0530

    Showing name

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,7 +5,7 @@ const person = (props) => {
       <div onClick={props.click}>
          <p>I'm {props.name} . And I am {props.age} years old!</p>
          <p>{props.children}</p>
-         <input type="text" onChange={props.changed}/>
+         <input type="text" onChange={props.changed} value={props.name}/>
       </div>
    )
 }
