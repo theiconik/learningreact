4c28c1eea442534c65d8e0539efa6541499bbbc9
theiconik <kumarujjawal51@gmail.com>
2021-03-16 23:16:24 +0530

    Styling component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './person.css';
 
 const person = (props) => {
    return (
-      <div onClick={props.click}>
+      <div className="Person" onClick={props.click}>
          <p>I'm {props.name} . And I am {props.age} years old!</p>
          <p>{props.children}</p>
          <input type="text" onChange={props.changed} value={props.name}/>
