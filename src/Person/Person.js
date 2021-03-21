dbd835e70e651cc6380dcf7c19490705ec85ca8d
theiconik <kumarujjawal51@gmail.com>
2021-03-22 02:17:39 +0530

    Used Styled-Component to style person card

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,20 +1,34 @@
 import React from 'react';
-import './person.css';
-import  Radium  from "radium";
+//import './person.css';
+//import  Radium  from "radium";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+      width: 60%;
+      margin: 16px auto;
+      border: 1px solid #eee;
+      box-shadow: 0 2px 3px #ccc;
+      padding: 16px;
+      text-align: center; 
+
+   @media (min-width: 500px) {
+      width: 450px; 
+   }
+`;
 
 const person = (props) => {
-   const style =  {
-      '@media (min-width: 500px)': {
-         width: '450px',
-      }
-   }
+   // const style =  {
+   //    '@media (min-width: 500px)': {
+   //       width: '450px',
+   //    }
+   // }
    return (
-      <div className="Person" onClick={props.click} style={style}>
+      <StyledDiv className="Person" onClick={props.click} /*style={style}*/>
          <p>I'm {props.name} . And I am {props.age} years old!</p>
          <p>{props.children}</p>
          <input type="text" onChange={props.changed} value={props.name}/>
-      </div>
+      </StyledDiv>
    )
 }
 
-export default Radium(person);
+export default person;
