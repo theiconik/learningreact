58d3165451be070cebcd30f7c8d2b489c98dc632
theiconik <kumarujjawal51@gmail.com>
2021-03-22 01:11:40 +0530

    Added media-query styling

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,8 +3,13 @@ import './person.css';
 import  Radium  from "radium";
 
 const person = (props) => {
+   const style =  {
+      '@media (min-width: 500px)': {
+         width: '450px',
+      }
+   }
    return (
-      <div className="Person" onClick={props.click}>
+      <div className="Person" onClick={props.click} style={style}>
          <p>I'm {props.name} . And I am {props.age} years old!</p>
          <p>{props.children}</p>
          <input type="text" onChange={props.changed} value={props.name}/>
