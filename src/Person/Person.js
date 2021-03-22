9096db9046433a72530ec9d60ff77587befe3050
theiconik <kumarujjawal51@gmail.com>
2021-03-22 13:20:25 +0530

    Media query using CSS Modules

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,20 +1,20 @@
 import React from 'react';
-//import './person.css';
+import classes from './person.css';
 //import  Radium  from "radium";
-import styled from "styled-components";
+//import styled from "styled-components";
 
-const StyledDiv = styled.div`
-      width: 60%;
-      margin: 16px auto;
-      border: 1px solid #eee;
-      box-shadow: 0 2px 3px #ccc;
-      padding: 16px;
-      text-align: center; 
+// const StyledDiv = styled.div`
+//       width: 60%;
+//       margin: 16px auto;
+//       border: 1px solid #eee;
+//       box-shadow: 0 2px 3px #ccc;
+//       padding: 16px;
+//       text-align: center; 
 
-   @media (min-width: 500px) {
-      width: 450px; 
-   }
-`;
+//    @media (min-width: 500px) {
+//       width: 450px; 
+//    }
+// `;
 
 const person = (props) => {
    // const style =  {
@@ -23,11 +23,11 @@ const person = (props) => {
    //    }
    // }
    return (
-      <StyledDiv className="Person" onClick={props.click} /*style={style}*/>
+      <div className={classes.Person} onClick={props.click} /*style={style}*/>
          <p>I'm {props.name} . And I am {props.age} years old!</p>
          <p>{props.children}</p>
          <input type="text" onChange={props.changed} value={props.name}/>
-      </StyledDiv>
+      </div>
    )
 }
 
