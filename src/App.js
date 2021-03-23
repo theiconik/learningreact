78cf2a51702481023f2fe2f7e1ecebedd014db80
theiconik <kumarujjawal51@gmail.com>
2021-03-23 19:25:01 +0530

    Comments fix

--- src/App.js
+++ src/App.js
@@ -116,11 +116,11 @@ class App extends Component {
 
     let assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.red); // classes = ['red']
+      assignedClasses.push(classes.red); // assignedClasses = ['red']
     }
 
     if (this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold); // classes = ['red', 'bold']
+      assignedClasses.push(classes.bold); // assignedClasses = ['red', 'bold']
     }
 
     return (
