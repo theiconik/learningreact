f0f055661b317d1995a525243f2e0beeab61fa20
theiconik <kumarujjawal51@gmail.com>
2021-03-23 23:37:38 +0530

    Wrapping App with Error Boundary

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import classes from "./App.css";
 import Person from "./Person/Person";
+import ErrorBoundary from "./Error/ErrorBoundary";
 //import Radium, { StyleRoot } from "radium";
 //import styled from "styled-components";
 
@@ -93,13 +94,14 @@ class App extends Component {
         <div>
           {this.state.persons.map((person, index) => {
             return (
+              <ErrorBoundary key={person.id}>
               <Person
                 name={person.name}
                 age={person.age}
                 click={() => this.deletePerson(index)}
-                key={person.id}
                 changed={(event) => this.nameChanged(event, person.id)}
               />
+              </ErrorBoundary>
             );
           })}
         </div>
